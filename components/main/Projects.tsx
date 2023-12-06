286bcb51a077df5deae0bc21f2b6dc5ba573fcd7
Lowrencyy <Lowrencyy@gmail.com>
2023-12-07 00:51:18 +0800

    final

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -26,6 +26,11 @@ const Projects = () => {
           title="Space Themed Website"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
         />
+        <ProjectCard
+          src="/SpaceWebsite.png"
+          title="Space Themed Website"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        />
         
       </div>
     </div>
